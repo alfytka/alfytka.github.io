c1d71febe81576de6aa066a760dd02959c99db65
alfytka <lyzexx04@gmail.com>
2023-05-08 16:56:40 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        figtree: ['Figtree'],
+      }
+    },
+  },
+  plugins: [],
+}
